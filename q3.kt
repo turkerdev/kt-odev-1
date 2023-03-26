eb86e5bb742b60d3e69d8b995c1ca1a751a05f2e
turkerdev <48413508+turkerdev@users.noreply.github.com>
2023-03-26 19:49:12 +0300

    fix q3

--- q3.kt
+++ q3.kt
@@ -2,12 +2,13 @@
 // kodunu fonksiyon kullanarak yazınız
 
 fun main() {
-    var num = 7
-    var res = 1
+    println(factorial(7))
+}
 
+fun factorial(num:Int):Int{
+    var res = 1
     for (i in num downTo 1) {
         res *= i
     }
-
-    println(res)
+    return res;
 }
